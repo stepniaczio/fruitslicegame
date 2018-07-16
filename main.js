fe5b6dca2a45f3bc02ca820ac68931f79e435657
Mike <32502190+stepniaczio@users.noreply.github.com>
2018-07-16 23:00:08 +0200

    Update main.js
    On a path to make this game object oriented

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 let playing = false;
 let score;
 let heartsLeft;
-let fruits = ['apple', 'cherries', 'mango', 'orange', 'peach', 'pineapple', 'raspberry', 'watermelon', 'banana'];
+//let fruits = ['apple', 'cherries', 'mango', 'orange', 'peach', 'pineapple', 'raspberry', 'watermelon', 'banana'];
 let step;
 let action;
 
@@ -17,68 +17,14 @@ $(function() {
 
             $('#hearts').show();
             heartsLeft = 3;
-            addHearts();
+            game.addHearts();
             $("#gameOver").hide();
             $('#startreset').html('Reset game');
-            startAction();
+            game.startAction();
 
         }
     });
 
-
-    function addHearts() {
-        $('#hearts').empty();
-        for (let i = 0; i < heartsLeft; i++) {
-            $('#hearts').append('<img src="images/heart.png" class="life">');
-        }
-    }
-
-
-    function startAction() {
-
-        generateNewFruit();
-
-        action = setInterval(function() {
-            $("#fruit1").css('top', $("#fruit1").position().top + step);
-            if ($("#fruit1").position().top > $("#fruitsContainer").height()) {
-                if (heartsLeft > 1) {
-                    generateNewFruit();
-                    heartsLeft--;
-                    addHearts();
-                } else {
-                    playing = false;
-                    $("#startreset").html("Start Game");
-                    $("#gameOver").show();
-                    $("#gameOver").html('<p>Game Over!</p><p>Your score is ' + score + '</p>');
-                    $("#hearts").hide();
-                    stopAction();
-                }
-            }
-        }, 10);
-    }
-
-
-    function chooseFruit() {
-        let randomNumber = Math.floor(Math.random() * fruits.length);
-        $('#fruit1').attr('src', 'images/' + fruits[randomNumber] + '.png');
-    }
-
-
-    function stopAction() {
-        clearInterval(action);
-        $("#fruit1").hide();
-    }
-
-
-    function generateNewFruit() {
-        $('#fruit1').show();
-        chooseFruit();
-        $('#fruit1').css({
-            'left': Math.round(550 * Math.random()),
-            'top': -50
-        });
-        step = Math.round(Math.random() * 5) + 1;
-    }
 
     $("#fruit1").mouseover(function() {
         score++;
@@ -88,8 +34,54 @@ $(function() {
         //stopAction();
         $("#fruit1").hide("explode", 200);
         //startAction();
-        setTimeout(startAction, 300);
+        setTimeout(game.startAction, 300);
     });
 
+    function generateNewFruit() {
+        $('#fruit1').show();
+        this.chooseFruit();
+        $('#fruit1').css({
+            'left': Math.round(550 * Math.random()),
+            'top': -50
+        });
+        let step = Math.round(Math.random() * 5) + 1;
+    }
 
+    const game = {
+        fruits: ['apple', 'cherries', 'mango', 'orange', 'peach', 'pineapple', 'raspberry', 'watermelon', 'banana'],
+        stopAction: function() {
+            clearInterval(action);
+            $("#fruit1").hide();
+        },
+        chooseFruit: function() {
+            let randomNumber = Math.floor(Math.random() * game.fruits.length);
+            $('#fruit1').attr('src', 'images/' + game.fruits[randomNumber] + '.png');
+        },
+        addHearts: function() {
+            $('#hearts').empty();
+            for (let i = 0; i < heartsLeft; i++) {
+                $('#hearts').append('<img src="images/heart.png" class="life">');
+            }
+        },
+        startAction: function() {
+            generateNewFruit();
+            let action = setInterval(function() {
+                $("#fruit1").css('top', $("#fruit1").position().top + step);
+                if ($("#fruit1").position().top > $("#fruitsContainer").height()) {
+                    if (heartsLeft > 1) {
+                        generateNewFruit();
+                        heartsLeft--;
+                        this.addHearts();
+                    } else {
+                        playing = false;
+                        $("#startreset").html("Start Game");
+                        $("#gameOver").show();
+                        $("#gameOver").html('<p>Game Over!</p><p>Your score is ' + score + '</p>');
+                        $("#hearts").hide();
+                        this.stopAction();
+                    }
+                }
+            }, 10);
+        },
+    }
 });
